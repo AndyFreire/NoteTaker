c4bb3d684f6049eb950abf6a17e80c207af6f79e
Andrew Freire <andrew.m.freire@gmail.com>
2020-09-26 15:25:39 -0500

    Completed Get and Post methods for notes

--- server.js
+++ server.js
@@ -31,10 +31,44 @@ app.get("/notes", function (req, res) {
 app.get("/api/notes", function (req, res) {
 
   fs.readFile("db/db.json", "utf8", function (err, data) {
-    console.log(data);
     res.json(JSON.parse(data));
   })
 
+});
+
+
+app.post("/api/notes", function (req, res) {
+  // Get the new note request
+  newNote = req.body
+
+  // get the old notes and store in savedNotes
+  var savedNotes = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+
+  // Add the new note to the savednotes
+  savedNotes.push(newNote)
+
+  // Write the old note back to the DB. Needed to stringify for this to work!
+  fs.writeFileSync("db/db.json", JSON.stringify(savedNotes));
+
+  // Return the new note as per the readme
+  res.json(newNote);
+});
+
+app.delete("/api/notes", function (req, res) {
+  // Get the new note request
+  id = req.body
+
+  // get the old notes and store in savedNotes
+  var savedNotes = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+
+  // Add the new note to the savednotes
+  savedNotes.filter(note => id !== note.id);
+
+  // Write the old note back to the DB. Needed to stringify for this to work!
+  fs.writeFileSync("db/db.json", JSON.stringify(savedNotes));
+
+  // Return the new note as per the readme
+  res.status({200: "OK"});
 });
 
 
